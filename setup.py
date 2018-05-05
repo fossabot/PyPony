213a6d132fea5dfce81be7385b6358f99321addb
harens <harensdeveloper@gmail.com>
2018-05-05 15:06:06 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+name='PyPony',
+version='0.9.0',
+description='ThePonyInformationProject in Python',
+long_description=ThePonyInformationProject aims to educate all about ponies. Now, it has come to Python!,
+long_description_content_type='text/x-rst',
+url='https://harens.github.io/ThePonyInformationProject/',
+author='harens',
+author_email='harensdeveloper@gmail.com',
+license='GPL-3.0',
+classifiers=[
+    'Development Status :: 4 - Beta',
+
+    'Intended Audience :: Pony Enthusiasts',
+    'Topic :: Ponies',
+
+    # Pick your license as you wish (should match "license" above)
+     'License :: GPL-3.0',
+
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+],
+keywords='Pony Information CreativeProject',
+project_urls={
+    'Source': 'https://github.com/harens/PyPony',
+    'CreativeProject': 'https://harens.github.io/CreativeProject/',
+    'Say Thanks!': 'https://saythanks.io/to/harens',
+    'Source': 'https://github.com/harens/PyPony',
+    'Tracker': 'https://github.com/harens/PyPony/issues',
+},
+python_requires='>=3',
