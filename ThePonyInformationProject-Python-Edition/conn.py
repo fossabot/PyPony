77d564e202cea0acaaf413b045444d5021ce0bd1
harens <harensdeveloper@gmail.com>
2018-05-05 14:11:46 +0100

    Add files via upload

---
+++ ThePonyInformationProject-Python-Edition/conn.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+print('''
+   _____                                                   _____            _
+  / ____|                                                 |  __ \          (_)
+ | |     ___  _ __  _ __   ___ _ __ ___   __ _ _ __ __ _  | |__) |__  _ __  _  ___  ___
+ | |    / _ \| '_ \| '_ \ / _ \ '_ ` _ \ / _` | '__/ _` | |  ___/ _ \| '_ \| |/ _ \/ __|
+ | |___| (_) | | | | | | |  __/ | | | | | (_| | | | (_| | | |  | (_) | | | | |  __/\__ \
+  \_____\___/|_| |_|_| |_|\___|_| |_| |_|\__,_|_|  \__,_| |_|   \___/|_| |_|_|\___||___/
+========================================================================================
+Connemara Ponies are around 128-148 cm tall.
+They take their name from a rocky and isolated part of West Ireland.
+One hypothesis of their origin is that the Vikings brought them in 795 AD.
+These ponies were first used by local farmers and labourers to bring large baskets of rock, and seaweed from the shore, to act as fertiliser.
+The Connemara Pony Breeders Society was created in 1923, with the English Connemara Society being established in 1947.
+Perhaps one of the best known of this breed is Stroller, who was ridden by Marion Mould, an English show-jumper born in 1947.
+The most magnificent performance of Connemara ponies takes place at the Clifden Show in County Galway on the third Thursday of every August.
+The principal use of the Connemara pony today is as a sports pony.
+========================================================================================
+Sources:
+http://www.horsebreedspictures.com/connemara-pony.asp
+https://www.discoverireland.ie/DI/media/DiscoverIreland/Places%20to%20Go/Connemara/PDFs/Irelands-West-Coast.pdf
+https://cpbs.ie/organisation/history/
+========================================================================================
+''')
+
+exec(open("menu.py").read())
---
+++ ThePonyInformationProject-Python-Edition/shet.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+print('''
+   _____ _          _   _                 _   _____            _
+  / ____| |        | | | |               | | |  __ \          (_)
+ | (___ | |__   ___| |_| | __ _ _ __   __| | | |__) |__  _ __  _  ___  ___
+  \___ \| '_ \ / _ \ __| |/ _` | '_ \ / _` | |  ___/ _ \| '_ \| |/ _ \/ __|
+  ____) | | | |  __/ |_| | (_| | | | | (_| | | |  | (_) | | | | |  __/\__ \
+ |_____/|_| |_|\___|\__|_|\__,_|_| |_|\__,_| |_|   \___/|_| |_|_|\___||___/
+===========================================================================
+Shetland ponies are around 107 cm tall after four years of age.
+Small ponies have existed in the Shetland Isles for over 2000 years.
+Numerous children used Shetland ponies for riding and driving.
+For their size, they are the strongest of all the horse breeds.
+When a law in 1847 banned children from entering the coal pits, the Shetland pony colts became in high demand.
+The Shetland Pony is thought to have descended from a cob-type pony from Southern Europe, and a pony brought to the Shetland Islands by the Celtic people.
+Several Shetland Ponies were owned by Queen Victoria to drive some of her carriages.
+The Shetland Pony Stud-Book Society was formed in 1890.
+Their coat changes according to the season, which helps protect them against the often harsh conditions of the islands
+The Independent wrote an article in 2016 stating that,"There are 50 ponies to every one human being on Foula Island [part of the Shetland islands]".
+===================================================================================================================
+Sources:
+http://www.shetlandponystudbooksociety.co.uk/about-the-breed
+http://ihearthorses.com/9-things-you-didnt-know-about-the-shetland-pony/
+https://www.shetland.org/things/explore-nature/shetland-ponies
+https://www.independent.co.uk/news/uk/home-news/scottish-island-has-more-miniature-horses-than-people-a7354861.html
+===================================================================================================================
+''')
+
+exec(open("menu.py").read())
